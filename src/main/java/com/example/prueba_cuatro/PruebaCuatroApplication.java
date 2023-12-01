cba31b16bd7d9c4047e355da112b2a4b680756fd
MONDAASESINA <josiasjoestar@hotmail.com>
2023-12-01 12:06:56 -0500

    una salida por pantalla

---
+++ src/main/java/com/example/prueba_cuatro/PruebaCuatroApplication.java
@@ -0,0 +1,15 @@
+package com.example.prueba_cuatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCuatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCuatroApplication.class, args);
+		System.out.println("ERROR SOLUCIONADO");
+		System.out.println("PRIMER COMMIT MANITO");
+	}
+
+}
---
+++ src/test/java/com/example/prueba_cuatro/PruebaCuatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_cuatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCuatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
